db37e397ca86a2fe870669f717ad709151f138bb
sang-3 <dbghdtkd12@naver.com>
2026-01-30 09:46:36 +0900

    온보딩 markup

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 export default function LandingPage() {
   return (
     <>
-      {/* <LandingContainer /> */}
-      <Link href="/records/new">기록추가 페이지</Link>
+      <LandingContainer />
+      {/* <Link href="/records/new">기록추가 페이지</Link>
       <div>
         <Link href="/records/detail">기록상세 페이지</Link>
       </div>
       <Link href="/records">기록 페이지</Link>
       <div>
         <Link href="/records/123/edit">기록 수정 페이지</Link>
-      </div>
+      </div> */}
     </>
   );
 }
