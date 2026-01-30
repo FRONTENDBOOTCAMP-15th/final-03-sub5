6efb057849d8b26b86044beb77f308bec430cc57
HyeJin003 <rkddkwnl003@gmail.com>
2026-01-30 16:35:34 +0900

    feat: #39 목표 레벨 더미와 기능 구현

--- app/goals/components/LevelHeader.tsx
+++ app/goals/components/LevelHeader.tsx
@@ -4,6 +4,8 @@ import { LevelInfo } from "../types";
 export default function LevelHeader() {
   const [level, setLevel] = useState([]);
 
+  const nowLevel = "고급";
+
   const LevelDummy: LevelInfo[] = [
     {
       level: "초급",
@@ -14,14 +16,14 @@ export default function LevelHeader() {
     },
     {
       level: "중급",
-      icon: "🌱",
+      icon: "🏃",
       pace: "6,00",
       totalDistance: 10,
       monthlyRuns: 3,
     },
     {
       level: "고급",
-      icon: "🌱",
+      icon: "🔥",
       pace: "8,00",
       totalDistance: 21,
       monthlyRuns: 3,
@@ -34,15 +36,27 @@ export default function LevelHeader() {
       monthlyRuns: 3,
     },
   ];
+  const leveling = LevelDummy.find((item) => {
+    if (item.level === nowLevel) {
+      return true;
+    }
+  });
+  console.log("==hi", leveling);
 
+  // const levels = leveling.map((item) => {
+  //   return {
+  //     level: item.level,
+  //     icon: item.icon,
+  //   };
+  // });
   return (
     <>
       {/* 탭 LevelIcon 상단 */}
       <section className="flex flex-col items-center">
-        <span className="text-3xl">🌱</span>
+        <span className="text-3xl"> {leveling?.icon || "정보 없음"}</span>
         <span className="mb-6 text-2xl">당신의 러닝 레벨</span>
         <span className="inline-block px-4 py-1 mb-2 bg-[#1FC0CC] rounded-full text-xs text-notselectbtn">
-          초급
+          {leveling?.level || "정보 없음"}
         </span>
       </section>
     </>
