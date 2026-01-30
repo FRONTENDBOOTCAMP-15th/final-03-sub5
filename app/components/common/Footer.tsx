ac6ac01b9b1363a1043b97fe3fad159c288ee5a5
matthewb2 <bestsciencekwabe@gmail.com>
2026-01-30 10:08:51 +0900

    Fix: 카피라이트 문구

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-white-300 items-center text-gray-400 text-xs gap-8">
+        Copyright Sub.5 All Rights Reserved
+      </div>
+    </>
+  );
 }
