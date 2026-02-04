fcb2908bb4ebd111860a1084b025349e0aeb5114
Jia-Oh <ojia7732@gmail.com>
2026-02-04 11:46:09 +0900

    내 게시글 목록 및 게시글 상세 페이지 기능 구현 중

--- app/profile/board/[id]/CommentItem.tsx
+++ app/profile/board/[id]/CommentItem.tsx
@@ -1,20 +1,22 @@
+import { Reply } from "@/types/post";
+
 // app/profile/board/[id]/CommentItem.tsx
-export default function CommentItem() {
+export default function CommentItem({ comment }: { comment: Reply }) {
   return (
-    <div className="border border-gray-200 rounded-md p-4 mb-3 bg-gray-50">
+    <div className="border border-gray-200 rounded-md p-4 bg-gray-50">
       {/* 작성 정보 */}
       <div className="flex justify-between items-center mb-2">
-        <span className="text-sm font-semibold text-gray-600">관리자</span>
+        <span className="text-sm font-semibold text-gray-600">
+          {comment.user.name}
+        </span>
         <time className="text-xs text-gray-400" dateTime="2026-01-05T14:11:22">
-          2026.01.05 14:11
+          {comment.createdAt}
         </time>
       </div>
 
       {/* 답변 내용 */}
       <p className="text-sm text-gray-700 leading-relaxed whitespace-pre-wrap">
-        안녕하세요. 문의 주신 내용 확인했습니다. 말씀해주신 현상은 현재
-        내부적으로 확인 중이며, 수정 완료 시 다시 안내드리겠습니다. 이용에
-        불편을 드려 죄송합니다.
+        {comment.content}
       </p>
     </div>
   );
